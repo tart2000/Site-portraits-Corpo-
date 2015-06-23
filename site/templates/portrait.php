8260f5e2d4d4b3733b88efdfafb2d8d4a6f62e8f
Arthur Schmitt <tart2000design@gmail.com>
2015-06-22 22:59:29 -0400

    better blockquotes

--- site/templates/portrait.php
+++ site/templates/portrait.php
@@ -18,33 +18,11 @@
           </div>
         </div>
         <div class="col-md-6">
-          <h1><?php echo $page->title()->html() ?></h1>
-          <hr>
-          <?php if ($page->companyLink() != '') : ?>
-            <a href="<?php echo $page->companyLink() ?>" target="_blank"><i class="fa fa-external-link company right"></i></a><h2><?php echo $page->company() ?></h2> 
-          <?php else : ?>
-            <h2><?php echo $page->company() ?></h2>
-          <?php endif ?>
-          <h3><?php echo $page->baseline() ?>.</h3>
-          <?php if ($page->children() != '') : ?>
-            <div class="flag">
-              <a href="<?php echo $page->children()->first()->url() ?>"><img src="/assets/images/flag_en.gif"></a>
-            </div>
-          <?php endif ?>
+          <?php snippet('portrait_header') ?>
         </div>
       <?php else : ?>
         <div class="col-md-8">
-          <h1><?php echo $page->title()->html() ?></h1>
-          <hr>
-          <?php if ($page->companyLink() != '') : ?>
-            <a href="<?php echo $page->companyLink() ?>" target="_blank"><i class="fa fa-external-link company right"></i></a><h2><?php echo $page->company() ?></h2> 
-          <?php endif ?>
-          <h3><?php echo $page->baseline() ?>.</h3>
-          <?php if ($page->children() != '') : ?>
-            <div class="flag">
-              <a href="<?php echo $page->children()->first()->url() ?>"><img src="/assets/images/flag_en.gif"></a>
-            </div>
-          <?php endif ?>
+          <?php snippet('portrait_header') ?>
         </div>
       <?php endif ?>
       <div class="col-md-1">
